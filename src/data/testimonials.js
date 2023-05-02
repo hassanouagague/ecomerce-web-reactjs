b6c590500bf980a1032683ca3b1d278853877edf
El Hasan Ouagague <89553958+hassanouagague@users.noreply.github.com>
2023-05-02 15:25:30 +0100

    Add files via upload

---
+++ src/data/testimonials.js
@@ -0,0 +1,40 @@
+import profile1 from '../assets/profile1.png'
+import profile2 from '../assets/profile2.png'
+import profile3 from '../assets/profile3.png'
+import profile4 from '../assets/profile4.png'
+import profile5 from '../assets/profile5.png'
+import profile6 from '../assets/profile6.png'
+
+
+export const TestimonialsData = [
+    {
+        image : profile1,
+        comment: " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        name: "John Alendon"
+    },
+    {
+        image : profile2,
+        comment: " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        name: "Natasha"
+    },
+    {
+        image : profile5,
+        comment: " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        name: "Alex Hormonzi"
+    },
+    {
+        image : profile4,
+        comment: " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        name: "John Alendon"
+    },
+    {
+        image : profile5,
+        comment: " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        name: "Natasha"
+    },
+    {
+        image : profile6,
+        comment: " Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        name: "Alex Hormonzi"
+    }
+]
